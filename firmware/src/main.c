f8d25bc25db0673e6e84703bdd4519a6611b7370
Alexandre Tuleu <alexandre.tuleu.2005@polyetchnique.org>
2013-12-03 01:11:12 +0000

    Sets a basic timer loop

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -1,127 +1,117 @@
-/*
-#include <avr/interrupt.h>
-#include <avr/wdt.h>
-#include <avr/power.h> 
-#include <avr/io.h>
-#include <util/delay.h>
-
-
-
-//#include "USB.h"
-//#include "GSG.h"
-
-
-volatile uint8_t systimeInMs;
-
-ISR(TIMER0_OVF_vect) {
-	PORTC ^= _BV(7);
-	//TCCR0B |= _BV(CS01) ;
-	//systimeInMs += 1;
-}
-
-
-/*uint16_t GetMillis() {
-	//we are on a 8 bit platform. Accessing a 16 byte value is never
-	//atomic !!! We should disable interrupt.
-	
-	uint16_t currentSystime;
-	
-	//disable Overflow interrupt
-	TIMSK1 &= ~_BV(OCIE1A);
-	
-	currentSystime = systimeInMs;
-
-	//enable interrupt
-	TIMSK1 |= _BV(OCIE1A);
-
-	return currentSystime;
-	}
-
-void SetupHardware() {
-	sei();
-	//disbale watchdog
-	//	MCUSR &= ~(1 << WDRF);
-	//wdt_disable();
-
-	//no prescaling
-	//clock_prescale_set(clock_div_1);
-   
-	DDRC = _BV(7); //Sets PortC Pin 7 as Digital Output.
-
-	//TCCR1A = 0x0; // no output compare std waveform
-	//TCCR0A |= _BV(WGM01); //CTC mode compare on OCR0A
-
-	//OCR0A = 249; // 16e6 /( 64 * (249 + 1) = 1e3 
-
-	//	systimeInMs = 0;
-
-	//this line with start the timer
-	TCCR0B |= _BV(CS01) | _BV(CS00) ;
-
-	TIMSK0 |= _BV(TOIE0); // Enables Timer Overflow Interrupt 1
-   
-   
-	PORTC |= _BV(7);
-}
-
-int main() {
-	SetupHardware();
-	//Initialize all our application
-	//	Init();
-	//starts the USB stack
-	//USB_Init();
-	
-
-	//uint16_t prevTime = GetMillis();
-
-
-	while(1) {
-		//		_delay_ms(500);
-		// PORTC ^= _BV(7);
-		//process the usb
-		//RunUSBTask();
-		
-		//process the app
-		//Process();
-
-		/*		int16_t curTime = GetMillis();
-		if(curTime - prevTime > 500 ) { 
-			prevTime += 500; //overflow will be keeped :-)
-
-			PORTC ^= _BV(7); //togle PORTC 7
-			}
-	}
-
-	return 0;
-}
-
-*/
-
-
 #include <avr/io.h> 
 #include <avr/interrupt.h>
+#include <util/delay.h> 
+#include <avr/power.h>
+#include <avr/wdt.h>
 
 #define val (65535 - 25000)
 
+volatile uint16_t systimeInMs;
+volatile uint16_t foo;
+
+ISR(TIMER0_COMPA_vect) {
+	++systimeInMs;
+}
+
+uint16_t GetMillis() {
+	uint16_t curTime;
+	uint8_t saveSREG = SREG;
+	cli();
+	curTime = systimeInMs;
+	SREG = saveSREG;
+	return curTime;
+}
+
+
+void LedDisplayTime(uint8_t sec) {
+	uint8_t saveSREG = SREG;
+	cli();
+
+	if( sec & ( 1 << 0 ) ) {
+		PORTC |= _BV(6);
+	} else {
+		PORTC &= ~_BV(6);
+	}
+	
+	if( sec & ( 1 << 1 ) ) {
+		PORTE |= _BV(6);
+	} else {
+		PORTE &= ~_BV(6);
+	}
+	
+	if( sec & ( 1 << 2 ) ) {
+			PORTB |= _BV(5);
+	} else {
+		PORTB &= ~_BV(5);
+	}
+	
+	if( sec & ( 1 << 3 ) ) {
+		PORTB |= _BV(7);
+	} else {
+		PORTB &= ~_BV(7);
+	}
+
+	SREG = saveSREG;
+}
+
 int main (void) { 
-	DDRC |= (1 << 7); // Set LED as output 
+	MCUSR &= ~_BV(WDRF);
+	wdt_disable();
+	
+	/* Disable clock division */
+	clock_prescale_set(clock_div_1);
 
-	TIMSK1 |= (1 << TOIE1); // Enable overflow interrupt 
-	sei(); // Enable global interrupts 
+	//disbale all usb realted interrupt
 
-	TCNT1 = val; // Preload timer with precalculated value 
+	
 
-	TCCR1B |= ((1 << CS10) | (1 << CS11)); // Set up timer at Fcpu/64 
 
-	PORTC ^= (1 << 7);
+	TIMSK1 = 0;
+	TIMSK0 |= _BV(OCIE0A); // Enable overflow interrupt 
 
-	for (;;){
-		
-	} 
+
+	TCNT0 = 0 ; 
+
+	systimeInMs = 0;
+
+	OCR0A = 249;
+
+	TCCR0A |= _BV(WGM01);
+
+	TCCR0B |= ((1 << CS01) | (1 << CS00)); // Set up timer at Fcpu/64 
+
+	//stuff set by Micro's bootloader crashing everything !!!
+	UDIEN &= ~(_BV(SUSPE) | _BV(EORSTE) );
+
+
+	sei();
+
+	DDRC  |= _BV(6);
+	DDRE  |= _BV(6);
+	DDRB  |= _BV(5);
+	DDRB  |= _BV(7);
+	DDRC  |= _BV(7);
+
+	
+
+
+	uint8_t secondEllapsed = 0;
+	uint16_t prevTime = GetMillis();
+
+	#define SEC_VALUE 1000
+
+	while(1) {
+		uint16_t curTime = GetMillis();
+		if(curTime - prevTime < SEC_VALUE ) {
+			continue;
+		}
+		prevTime += SEC_VALUE;
+		//_delay_ms(1000);
+		secondEllapsed += 1;
+		LedDisplayTime(secondEllapsed);
+	}
 }
  
-ISR(TIMER1_OVF_vect) { 
-	PORTC ^= (1 << 7); // Toggle the LED 
-	TCNT1  = val; // Reload timer with precalculated value 
-}
- 
+
+//ISR(USB_COM_vect){}
+//ISR(USB_GEN_vect){}
