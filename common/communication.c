ea8494594f85e72cfb7c74c76f5320895fb46497
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2013-12-21 21:09:13 +0100

    Updates the communication protocol

---
+++ common/communication.c
@@ -0,0 +1,21 @@
+#include "communication.h"
+
+#ifndef NULL 
+#define NULL ( (void*) 0 )
+#endif
+
+#ifdef __cplusplus
+extern "C"{
+#endif //__cplusplus
+
+GSGInstructionMetadata_t IMetaData[INST_NUMBER_OF_INSTRUCTION] =
+	{ { REQ_VENDOR , 2 * GSG_NUM_PARAMS , NULL }, // READ_ALL_PARAMS
+	  { REQ_VENDOR , 0 , NULL }, // SET_PARAM
+	  { REQ_VENDOR , 0 , NULL }, // SAVE_IN_EEPROM
+	  { REQ_VENDOR , 2 * 12 , NULL } // FETCH_CELL_VALUES
+	};
+
+
+#ifdef __cplusplus
+}
+#endif //__cplusplus
--- common/communication.h
+++ common/communication.h
@@ -2,95 +2,76 @@
 #define GSG_COMMON_COMMUNICATION_H_
 
 
+#include <stdint.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif //__cplusplus
 
 
-	// types of packet that can be send back from the GSG
-	typedef enum {
-		VI_TYPE_INVALID        = 0,
-		VI_TYPE_PARAM_RETURN   = 1,
-		VI_TYPE_CELL_RETURN    = 2,
-	} VendorInReportType_e;
+// All errors type return by the GSG
+typedef enum {
+	GSG_ERR_NO_ERROR = 0,
+	GSG_ERROR_MAX
+} VendorInError_e;
 
-	
 
-	// All errors type return by the GSG
-	typedef enum {
-		VI_ERR_NO_ERROR = 0,
-		VI_ERROR_MAX
-	} VendorInError_e;
+// Parameter for a load cell
+typedef enum {
+	CELL_TRESHOLD_UP          = 0,
+	CELL_TRESHOLD_DOWN        = 1,
+	//		CELL_PARAM_RESERVED_START = 2,
+	CELL_MAX_PARAMS           = 2,
+} GSGCellParam_e;
 
-	// A Packeted parameter, concatenation of An ID and a 16 bit value
-	typedef struct __attribute__((__packed__))  {
-		uint8_t ID;
-		uint16_t Value;
-	} GSGParameter_t;
-
-	// Parameter for a load cell
-	typedef enum {
-		CELL_TRESHOLD_UP          = 0,
-		CELL_TRESHOLD_DOWN        = 1,
-		//		CELL_PARAM_RESERVED_START = 2,
-		CELL_MAX_PARAMS           = 2,
-	} GSGCellParam_e;
-
-	// Lists all parameters for a GSG
-	typedef enum {
-		PARAM_NULL_ID  = 0,
-		LED_PERIOD     = 1,
-		CELL_1         = CELL_MAX_PARAMS * 1,  // 0x02
-		CELL_2         = CELL_MAX_PARAMS * 2,  // 0x04
-		CELL_3         = CELL_MAX_PARAMS * 3,  // 0x06
-		CELL_4         = CELL_MAX_PARAMS * 4,  // 0x08
-		CELL_5         = CELL_MAX_PARAMS * 5,  // 0x0a
-		CELL_6         = CELL_MAX_PARAMS * 6,  // 0x0c
-		CELL_7         = CELL_MAX_PARAMS * 7,  // 0x0e
-		CELL_8         = CELL_MAX_PARAMS * 8,  // 0x10
-		CELL_9         = CELL_MAX_PARAMS * 9,  // 0x12
-		CELL_10        = CELL_MAX_PARAMS * 10, // 0x14
-		CELL_11        = CELL_MAX_PARAMS * 11, // 0x16
-		CELL_12        = CELL_MAX_PARAMS * 12, // 0x18
-		GSG_NUM_PARAMS = CELL_MAX_PARAMS * 13, // 0x1a
-	} GSGParam_e;
+// Lists all parameters for a GSG
+typedef enum {
+	PARAM_NULL_ID  = 0,
+	LED_PERIOD     = 1,
+	CELL_1         = CELL_MAX_PARAMS * 1,  // 0x02
+	CELL_2         = CELL_MAX_PARAMS * 2,  // 0x04
+	CELL_3         = CELL_MAX_PARAMS * 3,  // 0x06
+	CELL_4         = CELL_MAX_PARAMS * 4,  // 0x08
+	CELL_5         = CELL_MAX_PARAMS * 5,  // 0x0a
+	CELL_6         = CELL_MAX_PARAMS * 6,  // 0x0c
+	CELL_7         = CELL_MAX_PARAMS * 7,  // 0x0e
+	CELL_8         = CELL_MAX_PARAMS * 8,  // 0x10
+	CELL_9         = CELL_MAX_PARAMS * 9,  // 0x12
+	CELL_10        = CELL_MAX_PARAMS * 10, // 0x14
+	CELL_11        = CELL_MAX_PARAMS * 11, // 0x16
+	CELL_12        = CELL_MAX_PARAMS * 12, // 0x18
+	GSG_NUM_PARAMS = CELL_MAX_PARAMS * 13, // 0x1a
+} GSGParam_e;
 	
 #define CELL_PARAM(nbCell,param) ( CELL_NUM_PARAMS * (nbCell) + (param))
 	
-	typedef enum {
-		INST_NONE              = 0,
-		INST_READ_PARAMS       = 1,
-		INST_SET_PARAMS        = 2,
-		INST_SAVE_IN_EEPROM    = 3,
-		INST_FETCH_CELL_VALUES = 4,
-		INST_MAX
-	} GSGHostInstruction_e;
+typedef enum {
+	INST_READ_ALL_PARAMS   = 0,
+	INST_SET_PARAM         = 1,
+	INST_SAVE_IN_EEPROM    = 2,
+	INST_FETCH_CELL_VALUES = 3,
+	INST_NUMBER_OF_INSTRUCTION
+} GSGHostInstruction_e;
 
-#define NB_CELLS 12
-#define NB_PARAMS 8
 
-	typedef struct __attribute__((__packed__)) { 
-		uint8_t type;
-		uint8_t error;
-		union {
-			uint16_t cells[NB_CELLS];
-			GSGParameter_t params[NB_PARAMS];
-		} data;
-	} VendorInReport_t;
-	
-	
-	typedef struct __attribute__((__packed__)) {
-		uint16_t  instructionID;		
-		GSGParameter_t params[NB_PARAMS];
-	} VendorOutReport_t;
-	
-	
-	
+
+typedef struct {
+	const uint8_t  bmRequestType;
+	const uint16_t wLength;
+	void *         userData;
+} GSGInstructionMetadata_t;
+
+extern GSGInstructionMetadata_t IMetaData[INST_NUMBER_OF_INSTRUCTION];
+
 #ifdef __cplusplus
 }
 #endif //__cplusplus
 
 
+#ifdef REQDIR_DEVICETOHOST
+#define REQ_VENDOR ( REQDIR_DEVICETOHOST | REQTYPE_VENDOR | REQREC_DEVICE )
+#else 
+#define REQ_VENDOR ( ( 1<< 7 ) | ( 2 << 5 ) | ( 0 << 0 ) )
+#endif 
 
 #endif //GSG_COMMON_COMMUNICATION_H_
